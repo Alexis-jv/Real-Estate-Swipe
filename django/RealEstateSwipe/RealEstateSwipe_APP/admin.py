83f99f72034a242e975bf515e7b87ec7c4a82c20
Jeannot NAEGELY <jeannot.naegely@etu.unistra.fr>
2023-05-16 18:35:17 +0200

    finished models + added REST modules and dependancies

--- django/RealEstateSwipe/RealEstateSwipe_APP/admin.py
+++ django/RealEstateSwipe/RealEstateSwipe_APP/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from RealEstateSwipe_APP.models import Message, Conversation, User, Property, Advantage
+from RealEstateSwipe_APP.models import Message, Conversation, User, Property, Advantage, Picture
 
 admin.site.register(Message)
 admin.site.register(Conversation)
 admin.site.register(User)
 admin.site.register(Property)
 admin.site.register(Advantage)
+admin.site.register(Picture)
--- django/RealEstateSwipe/RealEstateSwipe_APP/models.py
+++ django/RealEstateSwipe/RealEstateSwipe_APP/models.py
@@ -2,41 +2,15 @@ from django.db import models
 
 # Create your models here.
 
-class Advantage(models.Model): # DONE
+class Advantage(models.Model):
     label = models.CharField(max_length=500,null=False)
-    icon = models.CharField(max_length=500,null=False) # Not sure about this one, ask Alexis what it should be
+    icon = models.CharField(max_length=20,null=False)
 
     def __str__(self):
         return self.name
 
 
-class Conversation(models.Model):
-
-    def __str__(self):
-        return self.name
-
-
-class User(models.Model): # DONE
-    firstName = models.CharField(max_length=30,null=False)
-    lastName = models.CharField(max_length=30,null=False)
-    email = models.CharField(max_length=30,null=False)
-    password = models.CharField(max_length=30,null=False)
-
-    def __str__(self):
-        return self.name
-
-
-class Message(models.Model): # DONE
-    content = models.CharField(max_length=500,null=False)
-    date = models.DateField(auto_now_add=True,auto_now=False)
-    iduser = models.ForeignKey(User,null=False,on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
-
-
-
-class Property(models.Model): # DONE
+class Property(models.Model):
     label = models.CharField(max_length=20,null=False)
 
     class TypeChoice(models.TextChoices):
@@ -54,19 +28,51 @@ class Property(models.Model): # DONE
     nbrRoom = models.IntegerField(null=False)
     price = models.FloatField(null=False)
     isRental = models.BooleanField(default=True)
-    iduser = models.ForeignKey(User,null=False,on_delete=models.CASCADE)
+    # iduser = models.ForeignKey(User,null=False,on_delete=models.CASCADE)
     advantages = models.ManyToManyField(Advantage)
 
     def __str__(self):
         return self.name
 
 
-def upload_location(instance,filename,**kwargs):
-    file_path = 'django/RealEstateSwipe'
-
-
-class Picture(models.Model): # DONE
+class Picture(models.Model):
     photo = models.ImageField(upload_to='images/',null=False,blank=False)
+    idProperty = models.ForeignKey(Property,null=False,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class User(models.Model):
+    firstName = models.CharField(max_length=30,null=False)
+    lastName = models.CharField(max_length=30,null=False)
+    email = models.CharField(max_length=30,null=False)
+    password = models.CharField(max_length=30,null=False)
+    liked = models.ManyToManyField(Property,related_name='liked_properties')
+    passed = models.ManyToManyField(Property,related_name='disliked_properties')
+
+    def __str__(self):
+        return self.name
+
+
+try:
+    Property.add_to_class('idUser',models.ForeignKey(User,null=False,on_delete=models.CASCADE))
+except Exception as e:
+    raise e
+
+
+class Message(models.Model):
+    content = models.CharField(max_length=500,null=False)
+    date = models.DateField(auto_now_add=True,auto_now=False)
+    iduser = models.ForeignKey(User,null=False,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Conversation(models.Model):
+    idUser1 = models.ForeignKey(User,null=False,on_delete=models.CASCADE,related_name='first_user')
+    idUser2 = models.ForeignKey(User,null=False,on_delete=models.CASCADE,related_name='second_user')
     idProperty = models.ForeignKey(Property,null=False,on_delete=models.CASCADE)
 
     def __str__(self):
