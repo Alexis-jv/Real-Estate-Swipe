acd57df7c61882975b9a5439c5fe45479d9c24d4
Jeannot NAEGELY <jeannot.naegely@etu.unistra.fr>
2023-05-27 22:59:39 +0200

    Commented a work in progress code line to be able to run the server

--- django/RealEstateSwipe/RealEstateSwipe_APP/urls.py
+++ django/RealEstateSwipe/RealEstateSwipe_APP/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('messages/<idConv>/', rest_message_view, name="messages_rest"),
     path('pictures/<idProp>/', rest_picture_view, name="picture_rest"),
 
-    path('upload/property/', upload_property, name = 'upload_property'),
+    # Form testing
+    # path('upload/property/', upload_property, name = 'upload_property'),
 ]
