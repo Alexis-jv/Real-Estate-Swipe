39591664e2a72037a1795cfb045ab852e8d83217
hector <hx.euwx@gmail.com>
2023-05-31 21:24:12 +0200

    better settings screen !

--- navigation/screens/SettingsScreen.js
+++ navigation/screens/SettingsScreen.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect, useContext } from 'react';
-import { StyleSheet, Text, View, Switch } from 'react-native';
-import { ThemeContext } from './ThemeContext';
 
+
+import React, { useState, useEffect, useContext } from 'react';
+import { StyleSheet, Text, View, Switch, TouchableOpacity } from 'react-native';
+import { ThemeContext } from './ThemeContext';
 
 export default function SettingsScreen() {
   const [notificationsEnabled, setNotificationsEnabled] = useState(false);
@@ -16,35 +17,58 @@ export default function SettingsScreen() {
   };
 
   useEffect(() => {
+    // Do something when the dark mode value changes
   }, [darkModeEnabled]);
 
   return (
-    <View style={[styles.screen, darkModeEnabled && styles.darkScreen]}>
+    <View style={[styles.container, darkModeEnabled && styles.darkContainer]}>
       <View style={styles.settingItem}>
         <Text style={[styles.settingText, darkModeEnabled && styles.darkText]}>Enable Notifications</Text>
         <Switch
           value={notificationsEnabled}
           onValueChange={handleNotificationsToggle}
+          thumbColor={darkModeEnabled ? '#fff' : '#f4f3f4'}
+          trackColor={{ false: '#767577', true: '#81b0ff' }}
         />
       </View>
 
       <View style={styles.settingItem}>
         <Text style={[styles.settingText, darkModeEnabled && styles.darkText]}>Dark Mode</Text>
-        <Switch value={darkModeEnabled} onValueChange={handleDarkModeToggle} />
+        <Switch
+          value={darkModeEnabled}
+          onValueChange={handleDarkModeToggle}
+          thumbColor={darkModeEnabled ? '#fff' : '#f4f3f4'}
+          trackColor={{ false: '#767577', true: '#81b0ff' }}
+        />
       </View>
 
-      <Text style={[styles.sectionTitle, darkModeEnabled && styles.darkText]}>Account Settings</Text>
-      {/* Add more settings options related to the user's account here */}
+      <View style={styles.sectionContainer}>
+        <Text style={[styles.sectionTitle, darkModeEnabled && styles.darkText]}>Account Settings</Text>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Change Password</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Delete Account</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.sectionContainer}>
+        <Text style={[styles.sectionTitle, darkModeEnabled && styles.darkText]}>Privacy Settings</Text>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Manage Privacy Settings</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  screen: {
+  container: {
     flex: 1,
     padding: 16,
+    backgroundColor: '#fff',
   },
-  darkScreen: {
+  darkContainer: {
     backgroundColor: '#333',
   },
   settingItem: {
@@ -55,13 +79,30 @@ const styles = StyleSheet.create({
   },
   settingText: {
     fontSize: 16,
+    color: '#333',
   },
   darkText: {
-    color: 'white',
+    color: '#fff',
+  },
+  sectionContainer: {
+    marginBottom: 24,
   },
   sectionTitle: {
     fontSize: 18,
     fontWeight: 'bold',
-    marginTop: 16,
+    marginBottom: 16,
+    color: '#333',
+  },
+  button: {
+    backgroundColor: '#81b0ff',
+    borderRadius: 8,
+    padding: 12,
+    marginBottom: 8,
+  },
+  buttonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#fff',
+    textAlign: 'center',
   },
 });
